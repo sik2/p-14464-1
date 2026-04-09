9489ac3f0d75aed2e02790bdeb808335d8d1b1fb
sik2 <bickwang123@gmail.com>
2026-04-09 10:01:04 +0900

    0004

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
 
+        int lastId = 0;
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
@@ -21,7 +22,9 @@ public class Main {
                 System.out.print("작가 : ");
                 String wiseSayingAuthor = scanner.nextLine().trim();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                int num = ++lastId;
+//                System.out.printf("%d번 명언이 등록되었습니다. \n", num);
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(num));
             }
         }
 
